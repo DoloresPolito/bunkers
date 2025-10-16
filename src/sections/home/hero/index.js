330446e88c9f19cc698f4558473d1bd6df2f7087
Dolores Polito <dolores.polito@gmail.com>
2025-10-16 10:25:15 -0300

    hero background

---
+++ src/sections/home/hero/index.js
@@ -0,0 +1,50 @@
+"use client";
+import styles from "./styles.module.scss";
+import { slideUp2, slideUp3 } from "./animations";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import image from "../../../../public/background/background-bunkers.png";
+
+export default function Hero() {
+  return (
+    <div className={styles.section}>
+      <motion.div
+        className={styles.home}
+        variants={slideUp2}
+        initial="initial"
+        animate="enter"
+      >
+        <div className={styles.imageContainer}>
+          <Image
+            src={image}
+            alt="fondo"
+            fill
+            priority
+            style={{ objectFit: "cover" }}
+          />
+        </div>
+
+        <div className={styles.textContainer}>
+          <motion.h1
+            variants={slideUp3}
+            initial="initial"
+            animate="enter"
+            className={styles.title}
+          >
+            bunkers
+          </motion.h1>
+
+          <motion.p
+            variants={slideUp3}
+            initial="initial"
+            animate="enter"
+            className={styles.subtitle}
+          >
+            algo <span>nuevo</span> está llegando.
+          </motion.p>
+          <button className={styles.button}>info@bunkers.ar</button>
+        </div>
+      </motion.div>
+    </div>
+  );
+}
